18ad8095310b61e544d67a88d23445f49273f779
vesk <vesk@itu.dk>
2019-11-24 19:59:55 +0100

    lavede GUI med knap, main method virker ikke

---
+++ src/GUI_1.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_1 {
+    private JButton search_confirm;
+    private JPanel panel;
+
+    public GUI_1() {
+        search_confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Vi indlæser stadig filmene, vent venligst.");
+            }
+
+            public static void main(String[] args) {
+                JPanel p = new JPanel("search_button");
+                p.setContentPane(new GUI_1().panel);
+                p.setDefaultCloseOperation(JPanel.EXIT_ON_CLOSE);
+                p.pack();
+                p.setVisible(true);
+            }
+        });
+    }
+}
