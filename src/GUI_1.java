043f513153ea40630809f62f4d55ef0956a678e9
lippITU <lipp@itu.dk>
2019-11-25 21:55:15 +0100

    update comment

--- src/GUI_1.java
+++ src/GUI_1.java
@@ -21,7 +21,7 @@ public class GUI_1 {
             // derefter kan et JPanel tilføjes til containeren. Det ser dog ud til at du vil lave en knap?
             // --> lav en JButton, tilføj til dit JPanel, tilføj derefter JPanel til contentPane. kasser inde i kasser yo
             // det bliver hurtigt til rigtig meget kode - jeg synes vi skal bruge Scene Builder - det virker mere intuitivt, og så kan vi bruge javaFX (anbefalet af andre SWU'ere) 
-            // jeg skal nok hitte ud af hvordan man får prgrommet det til at snakke sammen med en controller klasse
+            // jeg skal nok hitte ud af hvordan man får programmet til at snakke sammen med en controller klasse
             // der ligger ret mange videoer om det på youtube, hvis man er interesseret
             // ps. jeg ved ikke hvor smart det er at have main(String[] args) metoden i GUI klassen.
             // det er som regel "entry point" til programmet (starter hele molevitten) - jeg ville måske kalde en "opret GUI" metode i main metoden, et andet sted
