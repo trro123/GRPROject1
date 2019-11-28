5627d86230749fd2146ebb37516e8f0567578154
trro <itu@trro.dk>
2019-11-28 14:39:00 +0100

    Added MediaContainer Class - with methods

---
+++ src/MediaContainer.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MediaContainer {
+    protected ArrayList<Series> series;
+    protected ArrayList<Movie> movies;
+    protected Series serie;
+    protected Movie movie;
+    public void addSeries(){
+        series.add(serie);
+    }
+    public void addMovie(){
+        movies.add(movie);
+    }
+}
--- src/Series.java
+++ src/Series.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
+
 public class Series extends Watchable{
     protected int seasons;
     protected int endYear;
-
     public Series(String title, int year, int endYear, double rating, int seasons, String[] genres){
         super(title, year, rating, genres);
         this.endYear = endYear;
