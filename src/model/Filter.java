9a2d6383b4ca0c7258a7d0b1633162073b89939d
lippITU <lipp@itu.dk>
2019-11-29 02:42:06 +0100

    update comment

--- src/model/Filter.java
+++ src/model/Filter.java
@@ -1,6 +1,6 @@
 package model;
 
-//det var måske noget hø det jeg sagde på telefonen i går. Det er nok nemmere at placere filter metoderne ind i MediaContainer klassen,
+//det var måske noget hø det jeg sagde på telefonen i går. Det er nok nemmere at placere filter metoderne inde i MediaContainer klassen,
 // da de så vil have tilgang til hhv. listen af film og serier for det enkelte mediacontainer objekt.
 // kh mikeli
 public class Filter {
