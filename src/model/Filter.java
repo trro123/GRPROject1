219cd20b92341f1ed34d280a8f09b539b7006581
lippITU <lipp@itu.dk>
2019-12-02 14:24:27 +0100

    oprydning

--- src/model/Filter.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-//det var måske noget hø det jeg sagde på telefonen i går. Det er nok nemmere at placere filter metoderne inde i MediaContainer klassen,
-// da de så vil have tilgang til hhv. listen af film og serier for det enkelte mediacontainer objekt. Men jeg er stadig lidt usikker :)
-// kh mikeli
-public class Filter {
-}
