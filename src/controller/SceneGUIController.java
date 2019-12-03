15db0ff88e128c8d4b9cf7360f9f0f3b20d6052d
trro <itu@trro.dk>
2019-12-03 14:02:50 +0100

    trynna make shit work

---
+++ src/controller/SceneGUIController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class SceneGUIController {
+    @FXML
+    public Font x1;
+    @FXML
+    public Color x2;
+    @FXML
+    public Font x3;
+    @FXML
+    public Color x4;
+}
