88c4d2cdad8b0472872d157cd4340b2990720ee1
lippITU <lipp@itu.dk>
2019-12-03 11:18:56 +0100

    add method to find X amount of movies/series in a specific genre

---
+++ src/test/howMany.java
@@ -0,0 +1,25 @@
+package test;
+
+import model.*;
+
+public class howMany{
+
+    public static void main(String[] args) {
+        MediaContainer medias = new MediaContainer();
+        try{
+            medias.loadMovies();
+            medias.loadSeries();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        int counter = 0;
+        for(Series m : medias.getSeries()){
+            if(m.getGenres().contains("Drama")){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+
+}
