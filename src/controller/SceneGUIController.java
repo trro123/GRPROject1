5e561d9aa23420a2ca355d3014c628004eb47f8f
lippITU <lipp@itu.dk>
2019-12-05 16:05:22 +0100

    oprydning

--- src/controller/SceneGUIController.java
+++ src/controller/SceneGUIController.java
@@ -33,13 +33,11 @@ public class SceneGUIController implements Initializable  {
     @FXML
     Label label;
     @FXML
-    TilePane tilePane; //definere det tilepane som ER oprettet i Scenebuilder.
+    TilePane tilePane; //definerer det tilepane som ER oprettet i Scenebuilder.
     @FXML
-    AnchorPane myAnchor; //definere det tilepane som ER oprettet i Scenebuilder.
+    AnchorPane myAnchor; //definerer det tilepane som ER oprettet i Scenebuilder.
     //@FXML
     //ScrollPane scrollPane;
-
-    int count = 0;
 
     private int nRows = 10; //number of rows on tilepane
     private int nCols = 10; //number of column for tile pane (dette skal skaleres op senere hen)
@@ -59,7 +57,6 @@ public class SceneGUIController implements Initializable  {
         // i stedet for at indlæse en ekstern fil, så evt. kald en metode, der tager en genre-parameter
         // og lister filmene på baggrund af den
 
-
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog(parent);
 
@@ -75,9 +72,11 @@ public class SceneGUIController implements Initializable  {
 
         createElements();
     }
+
     private void createElements(){
         tilePane.getChildren().clear();
 
+        int count = 0;
         for (int i = 0; i< nCols; i++){
             for (int j = 0; j < nRows; j++){ //herinde tænker jeg vi kan blive ved med at lave nye rows, så længe j er mindre end files.length()?/files.size
                 tilePane.getChildren().add(createPage(count));
@@ -86,21 +85,20 @@ public class SceneGUIController implements Initializable  {
         }
     }
     public VBox createPage(int index){
-
         ImageView imageView = new ImageView();
 
         // i stedet for at gemme billeder i det her array, så gennemløb media-listen fra Model og hiv billederne ud derfra
 
-       /* ArrayList<Media> mediaList = MediaContainer.givMigAlleDineMediaObjekterIEnArrayList(); //man bør have begge typer medier på samme liste, og så lave if-statements hvis der skal skelnes mellem de to-
+        /*
+        ArrayList<Media> mediaList = MediaContainer.givMigAlleDineMediaObjekterIEnArrayList(); //man bør have begge typer medier på samme liste, og så lave if-statements hvis der skal skelnes mellem de to-
 
         for (Media media : mediaList) {
             if (media.getGenres().equals("enBestemtGenre")) {
                 media.getImg();
             }
         }
-
-
         */
+
         File file = filesJpg[index];
         try{
             BufferedImage bufferedImage = ImageIO.read(file);
@@ -112,25 +110,24 @@ public class SceneGUIController implements Initializable  {
 
             imageView.setSmooth(true);
             imageView.setCache(true);
-        }
-        catch (IOException e){
+        } catch (IOException e){
             e.printStackTrace();
         }
+
         VBox pageBox = new VBox();
         pageBox.getChildren().add(imageView);
         pageBox.setStyle("-fx-border-color: orange;");
         return pageBox;
-    }
-    public void initialize(URL url, ResourceBundle rb) {
 
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
         tilePane.setPrefColumns(nCols);
         tilePane.setPrefRows(nRows);
         myAnchor.setStyle("-fx-background-image: Backgound.png;"); //-fx-background-color: rgba(0, 0, 0);
         //tilePane.setStyle("-fx-background-color: rgba(0, 0, 0);");
         tilePane.setHgap(GAP);
         tilePane.setVgap(GAP);
-
-
     }
 }
 
