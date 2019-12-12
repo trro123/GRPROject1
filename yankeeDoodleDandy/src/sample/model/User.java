4339d226c29da03bd72f84207332c409656fc489
lippITU <lipp@itu.dk>
2019-12-12 14:31:29 +0100

    welp

---
+++ yankeeDoodleDandy/src/sample/model/User.java
@@ -0,0 +1,41 @@
+package sample.model;
+
+import java.util.*;
+
+public class User{
+    private ArrayList<Watchable> watchlist;
+    private String username;
+    private String password;
+    
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        watchlist = new ArrayList<>();
+    }
+
+    public boolean checkPassword(String input){
+        if(input == password){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+// get methods
+    public ArrayList<Watchable> getWatchlist(){
+        return watchlist;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+// mutator methods
+    public void addToWatchlist(Watchable m){
+        watchlist.add(m);
+    }
+
+    public void removeFromWatchlist(Watchable m){
+        watchlist.remove(m);
+    }
+}
