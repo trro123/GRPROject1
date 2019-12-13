be284e82fa3b8e5d17713706c81f00514f3e1545
vesk <vesk@itu.dk>
2019-12-13 16:17:56 +0100

    cancel virker :D

--- src/view/CreateUserController.java
+++ src/view/CreateUserController.java
@@ -1,5 +1,18 @@
 package view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CreateUserController {
-    
+
+    public void cancel(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent loginPage = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        app_stage.hide();
+    }
 }
