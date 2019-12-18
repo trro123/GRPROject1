576b57103aec9b513e419ca6d839f0748d8af24e
lippITU <lipp@itu.dk>
2019-12-18 12:22:38 +0100

    exceptions

--- yankeeDoodleDandy/src/sample/model/UsernameTakenException.java
+++ yankeeDoodleDandy/src/sample/model/UsernameTakenException.java
@@ -12,7 +12,7 @@ public class UsernameTakenException extends RuntimeException {
 
         final Stage error = new Stage();
         error.initModality(Modality.APPLICATION_MODAL);
-        Label errorLabel = new Label("Woops, that username is already taken!");
+        Label errorLabel = new Label("ERROR 05; that username is already taken!");
         Scene dialogScene = new Scene(errorLabel, 300, 100);
         error.setScene(dialogScene);
         error.show();
